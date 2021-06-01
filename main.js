ed1fb4a643d077d7c05ed806d11ae7c0e957ca22
Thiago Gaitkoski <68345301+ThiagoGaitkoski@users.noreply.github.com>
2021-06-01 18:19:50 -0300

    Refatoração total

--- main.js
+++ main.js
@@ -1,74 +1,170 @@
-document.getElementById('formulario').addEventListener('submit', cadastraVeiculo)//Recebe o form e chama o cadastro
+document.getElementById('formulario').addEventListener('submit', cadastrarCarro);//Recebe o formulario do front
 
-function cadastraVeiculo(e){
-    let modeloCarro = document.getElementById('modeloCarro').value;
-    let placaCarro = document.getElementById('placaCarro').value;
-    let precoHora = document.getElementById('precoHora').value;
-    let horaEntrada = new Date();
+function cadastrarCarro(e){
+	let modeloCarro = document.getElementById('modeloCarro').value.trim().toUpperCase();
+	let placaCarro = document.getElementById('placaCarro').value.trim().toUpperCase();
+	let precoHora = document.getElementById('precoHora').value;
+	let horaEntrada = new Date();
 
-    carro = {
+	// Cria objeto com os dados preenchidos
+	var veiculo = {
 		modelo: modeloCarro,
 		placa: placaCarro,
 		preco: precoHora,
 		hora: horaEntrada.getHours(),
-		minutos: horaEntrada.getMinutes() 
-    }
+		minutos: horaEntrada.getMinutes() 	
+	};
 
-    if(localStorage.getItem('patio') === null){
-        let carros = [];
-        carros.push(carro);
-        localStorage.setItem('patio', JSON.stringify(carros));//Chega como objeto transforma em string
-    }else{
-        let carros = JSON.parse(localStorage.getItem('patio'));
-        carros.push(carro);
-        localStorage.setItem('patio', JSON.stringify(carros));
-    }
-    document.getElementById('formulario').reset();
+	if(localStorage.getItem('patio') === null){
+		let veiculos = [];
+		veiculos.push(veiculo);
+		localStorage.setItem('patio', JSON.stringify(veiculos));//transforma em string
+	} else {
+		let veiculos = JSON.parse(localStorage.getItem('patio'));
+		veiculos.push(veiculo);
+		localStorage.setItem('patio', JSON.stringify(veiculos));
+	}
 
-    mostraPatio();
-    console.log(carro);
-    e.preventDefault();
+	document.getElementById('formulario').reset();//Reseta as informações do formulário
+	mostraPatio();
+	e.preventDefault();
 }
 
-function apagarCarro(placa){
-    let carros = JSON.parse(localStorage.getItem('patio'));
-    let patioOcupado = document.getElementById('ocupadas');
-    let patioLivre = document.getElementById('patio');
-    let vagaOcupada = 1;
-    let vagaLivre = 9;
+function confirma(placa){
+	let c = confirm("Deseja realmente excluir?");
+	if (c) {
+		removeVeiculo(placa);
+		console.log('Carro removido!');
+	} else {
+		console.log('Carro não removido!');
+		return false;
+	}
+}
 
-    for(var i = 0; i < carros.length; i++){
-        if(carros[i].placa == placa){
-            carros.splice(i, 1);
-            patioLivre.innerHTML  = vagaLivre++;
-            patioOcupado.innerHTML = vagaOcupada--;
-        }
 
-        localStorage.setItem('patio', JSON.stringify(carros));
-    }
+function removeVeiculo(placa){
+	let patio = JSON.parse(localStorage.getItem('patio'));
 
-    mostraPatio();
+	for(var i = 0 ; i < patio.length; i++){
+		if(patio[i].placa == placa){
+			patio.splice(i, 1);
+		}
+	}
+
+	localStorage.setItem('patio', JSON.stringify(patio));
+	mostraPatio();
+}
+
+function calculaHoras(placa, preco, horas, minutos){
+	let tot = 0;
+	let valor = parseInt(preco);
+	let hr = parseInt(horas);
+	let min = parseInt(minutos);
+
+	let total = document.getElementById('total');
+
+	let date = new Date();
+	let horaAtual = parseInt(date.getHours());
+	let minAtual = parseInt(date.getMinutes());
+
+	let qtdHoras = horaAtual - hr;
+
+	let qtdMinutos = minAtual - min;
+
+	let precoHora = qtdHoras  *valor;
+
+	let precoMin = (qtdMinutos/60) * valor;
+
+	if (precoHora == 0){
+		tot = precoMin.toFixed(2);
+		
+	} else {
+		tot = (precoHora + precoMin).toFixed(2);
+	}
+
+	total.style.border = " 2px solid black";
+
+
+
+	total.innerHTML ='Pagamento referente a esse carro R$: ' + tot;
+	
+}
+
+function mostrarTotal(){
+
+}
+
+function mostrarTudo(num){
+	
+	if (num ==1){
+		mostraPatio();		
+	}
+	document.getElementById('total').innerHTML = '';
+	document.getElementById('tabela').style.display = "block";
+	document.getElementById('esconde').style.display = "inline-block";
+	document.getElementById('mostra').style.display = "none";
+	document.getElementById('total').style.display = "block";
+}
+
+function escondeTudo(){
+	document.getElementById('tabela').style.display = "none";
+	document.getElementById('mostra').style.display = "inline-block";
+	document.getElementById('esconde').style.display = "none";
+	document.getElementById('total').style.display = "none";
+}
+
+function pesquisar(){	
+	let pesq = document.getElementById('procurar').value.trim().toUpperCase();
+	veiculos = JSON.parse(localStorage.getItem('patio'));
+
+	let patioResultado = document.getElementById('resultados');
+
+	for (var i = 0; i < veiculos.length; i++){
+		if (pesq == veiculos[i].placa) {
+			let modelo = veiculos[i].modelo;
+			let placa = veiculos[i].placa;
+			let preco = veiculos[i].preco;
+			let hora = veiculos[i].hora;
+			let minutos = veiculos[i].minutos;
+			patioResultado.innerHTML = '<tr><td>'+ modelo + '</td>'+
+			'<td>'+ placa + '</td>' +
+			'<td>'+ preco + '</td>' +
+			'<td>'+ hora + ':' + minutos + '</td>' +
+			'<td><button type="button" class="" onclick="calculaHoras(\''+ placa +'\',\''+ preco +'\',\''+ hora +'\',\''+ minutos +'\')">Calcular</button></td>'+
+			'<td><button type="button" class="" onclick="confirma(\'' + placa + '\')">Finalizar</button></td>'+
+			'</tr>';
+
+			document.getElementById('procurar').innerHTML = "";
+			document.getElementById('procurar').style.border= "none";
+			mostrarTudo(0);
+			return true;
+		} else{
+			document.getElementById('procurar').style.border= "2px solid red";
+			alert('Não existe');
+			return false;
+		}
+
+	}
 }
 
 function mostraPatio(){
-    let carros = JSON.parse(localStorage.getItem('patio')); //Chamada dos carros que existem no patio
-    let patioResultado = document.getElementById('resultados');
-    let patioOcupado = document.getElementById('ocupadas');
-    let patioLivre = document.getElementById('patio');
-    let vagaOcupada = 1;
-    let vagaLivre = 9;
-    patioResultado.innerHTML = '';
-    patioLivre.innerHTML = '';
-    patioOcupado.innerHTML = '';
+	let veiculos = JSON.parse(localStorage.getItem('patio'));
+	let patioResultado = document.getElementById('resultados');
 
-    for(var i = 0; i < carros.length; i++){
-        let modelo = carros[i].modelo;
-        let placa = carros[i].placa;
-        let hora = carros[i].hora;
-        let minutos = carros[i].minutos;
+	patioResultado.innerHTML = '';
 
-        patioResultado.innerHTML += '<tr><td>'+ modelo +'</td><td>'+ placa +'</td><td>'+ hora +' : ' + minutos + '</td><td><button onclick="apagarCarro(\''+ placa +'\')">Excluir</button></td>'+'</tr>';
-        patioLivre.innerHTML  = vagaLivre--;
-        patioOcupado.innerHTML = vagaOcupada++;
-    }              
+	for(var i = 0; i < veiculos.length; i++){
+		let modelo = veiculos[i].modelo;
+		let placa = veiculos[i].placa;
+		let preco = veiculos[i].preco;
+		let hora = veiculos[i].hora;
+		let minutos = veiculos[i].minutos;
+		patioResultado.innerHTML += '<tr><td>'+ modelo + '</td>'+
+		'<td>'+ placa + '</td>' +
+		'<td>'+ preco + '</td>' +
+		'<td>'+ hora + ':' + minutos + '</td>' +
+		'<td><button type="button" class="" onclick="calculaHoras(\''+ placa +'\',\''+ preco +'\',\''+ hora +'\',\''+ minutos +'\')">Calcular</button></td>'+
+		'<td><button type="button" class="" onclick="confirma(\'' + placa + '\')" >Finalizar</button></td>'+
+		'</tr>';
+	}
 }
