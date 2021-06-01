c44b62ea70fa7b72bc1b44fd9b86972a8a0b05a0
Thiago Gaitkoski <68345301+ThiagoGaitkoski@users.noreply.github.com>
2021-06-01 18:44:10 -0300

    Ver erro de procura

--- main.js
+++ main.js
@@ -59,8 +59,7 @@ function calculaHoras(placa, preco, horas, minutos){
 	let tot = 0;
 	let valor = parseInt(preco);
 	let hr = parseInt(horas);
-	let min = parseInt(minutos);
-
+	let min = parseInt(minutos)
 	let total = document.getElementById('total');
 
 	let date = new Date();
@@ -68,11 +67,8 @@ function calculaHoras(placa, preco, horas, minutos){
 	let minAtual = parseInt(date.getMinutes());
 
 	let qtdHoras = horaAtual - hr;
-
 	let qtdMinutos = minAtual - min;
-
 	let precoHora = qtdHoras  *valor;
-
 	let precoMin = (qtdMinutos/60) * valor;
 
 	if (precoHora == 0){
@@ -82,45 +78,23 @@ function calculaHoras(placa, preco, horas, minutos){
 		tot = (precoHora + precoMin).toFixed(2);
 	}
 
-	total.style.border = " 2px solid black";
-
-
-
-	total.innerHTML ='Pagamento referente a esse carro R$: ' + tot;
-	
-}
-
-function mostrarTotal(){
-
+	total.style.border = " 2px solid green";
+	total.innerHTML ='Pagamento referente a esse carro R$:' + tot;
 }
 
 function mostrarTudo(num){
-	
-	if (num ==1){
+	if (num == 1){
 		mostraPatio();		
 	}
-	document.getElementById('total').innerHTML = '';
-	document.getElementById('tabela').style.display = "block";
-	document.getElementById('esconde').style.display = "inline-block";
-	document.getElementById('mostra').style.display = "none";
-	document.getElementById('total').style.display = "block";
-}
-
-function escondeTudo(){
-	document.getElementById('tabela').style.display = "none";
-	document.getElementById('mostra').style.display = "inline-block";
-	document.getElementById('esconde').style.display = "none";
-	document.getElementById('total').style.display = "none";
 }
 
 function pesquisar(){	
 	let pesq = document.getElementById('procurar').value.trim().toUpperCase();
 	veiculos = JSON.parse(localStorage.getItem('patio'));
-
 	let patioResultado = document.getElementById('resultados');
 
 	for (var i = 0; i < veiculos.length; i++){
-		if (pesq == veiculos[i].placa) {
+		if (pesq == veiculos[i].placa){
 			let modelo = veiculos[i].modelo;
 			let placa = veiculos[i].placa;
 			let preco = veiculos[i].preco;
@@ -133,19 +107,16 @@ function pesquisar(){
 			'<td><button type="button" class="" onclick="calculaHoras(\''+ placa +'\',\''+ preco +'\',\''+ hora +'\',\''+ minutos +'\')">Calcular</button></td>'+
 			'<td><button type="button" class="" onclick="confirma(\'' + placa + '\')">Finalizar</button></td>'+
 			'</tr>';
-
-			document.getElementById('procurar').innerHTML = "";
-			document.getElementById('procurar').style.border= "none";
-			mostrarTudo(0);
-			return true;
-		} else{
+			document.getElementById('procurar').style.border= "2px solid green";
+            mostrarTudo(0);
+		}else{
 			document.getElementById('procurar').style.border= "2px solid red";
-			alert('Não existe');
-			return false;
+			alert('Carro não encontrado');
 		}
 
 	}
 }
+
 
 function mostraPatio(){
 	let veiculos = JSON.parse(localStorage.getItem('patio'));
